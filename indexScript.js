140140e2e470238b77bdec354142235f7588e27c
thehg0902 <38461305+thehg0902@users.noreply.github.com>
2021-03-29 21:34:04 -0400

    Add files via upload

---
+++ indexScript.js
@@ -0,0 +1,7 @@
+function start(){
+  console.log("hello");
+  document.getElementById('navigation').scrollIntoView({behavior:"smooth"});
+}
+function page1(){
+  document.getElementById('page1').href="page1.html";
+}
